3ac66687b309310af9bb34a27c3bcf51c9491eb4
Tanay Desai <t2desai@uwaterloo.ca>
2020-07-10 14:26:07 -0400

    initial commit

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,36 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.history = []
+        self.index = 0
+        self.history.append(self.value)
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.history.append(self.value)
+        self.index += 1
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.history.append(self.value)
+        self.index += 1
 
     def undo(self):
-        pass
+        if self.index == 0:
+            return
+        self.index -= 1
+        self.value = self.history[self.index]
 
     def redo(self):
-        pass
+        if self.index == len(self.history) - 1:
+            return
+        self.index += 1
+        self.value = self.history[self.index]
 
     def bulk_undo(self, steps: int):
-        pass
+        self.index = max(0, self.index - steps)
+        self.value = self.history[self.index]
 
     def bulk_redo(self, steps: int):
-        pass
+        self.index = min(len(self.history) - 1, self.index + steps)
+        self.value = self.history[self.index]
